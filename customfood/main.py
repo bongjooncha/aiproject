88a33e77804bd419ddc58a15d2acf0501400958b
[AI9] 차봉준 <cbj0328@gmail.com>
2024-01-24 22:18:16 +0900

    barch, lr0수정

---
+++ customfood/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+import wandb
+
+if __name__ == '__main__':
+    wandb.init(project="foodyolo")
+
+    model = YOLO('yolov8n.pt')
+    model.train(data='./data.yaml', epochs=5, batch=64, save_dir='./', lr0=0.01)
+    model.val()
--- customfood/train.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-import wandb
-
-
-wandb.init(project="foodyolo")
-
-model = YOLO('yolov8n.pt')
-model.train(data='./data.yaml', epochs=100, patience=30, batch=64, save_dir='C:/Users/OWNER/Desktop/coding_project/aiproject/customfood/')
-model.val()
