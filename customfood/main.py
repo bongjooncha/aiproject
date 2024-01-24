2be75b0be76c1e2d6dee1f8be70c5245f2ef96ef
hiddenbebb <cbj4133@naver.com>
2024-01-24 23:49:43 +0900

    batch lr

--- customfood/main.py
+++ customfood/main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     wandb.init(project="foodyolo")
 
     model = YOLO('yolov8n.pt')
-    model.train(data='./data.yaml', epochs=5, batch=64, save_dir='./', lr0=0.01)
+    model.train(data='./data.yaml', epochs=5, batch=48, save_dir='./', lr0=0.01)
     model.val()
